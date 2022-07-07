eb3751282ff54ad3e1f63e250b040a69624f3bf9
EdenElbaz <edenelbaz1@mail.tau.ac.il>
2022-07-07 17:40:19 +0300

    eden's final updates

--- src/transition_detection/main.py
+++ src/transition_detection/main.py
@@ -1,14 +1,3 @@
-import scipy
-from scipy import io
-import os
-import mat73
-from os.path import dirname, join as pjoin
-import scipy.io as sio
-import pandas as pd
-import numpy as np
-import scipy.signal
-import matplotlib.pyplot as plt
-
 from src.transition_detection.files_lists import files_lists
 from src.transition_detection.load_data import load_data
 from src.transition_detection.preprocessing import preprocessing
@@ -20,50 +9,79 @@ from src.transition_detection.delete_PT_next_to_lying import delete_PT_next_to_l
 from src.transition_detection.elimination1 import elimination1
 from src.transition_detection.sitting_segements_fitting_standards import sitting_segements_fitting_standards
 from src.transition_detection.find_sit_to_stand import find_sit_to_stand
-from src.transition_detection.performance_detection import performance_detection
-
-
-listing_acc_path= (r'C:\Users\eden\Desktop\Axivity Lumbar vs thigh\Mat files\acc')
-listing_gait_path=(r'C:\Users\eden\Desktop\Axivity Lumbar vs thigh\Posture\Gait')
-listing_lying_path=(r'C:\Users\eden\Desktop\Axivity Lumbar vs thigh\Posture\Lying')
-
-
-(listing_acc,listing_gait,listing_lying,listing_lumbar,listing_thigh)= files_lists(listing_acc_path,listing_gait_path,listing_lying_path)
-
-for ff in range(len(listing_lumbar)):
-    (lumbar_acc, walking_vec_Lumbar, lying_vec_lumbar, thigh_acc, walking_vec_thigh, laying_vec_thigh) = load_data(ff,listing_acc_path,listing_gait_path,listing_lying_path,listing_acc,listing_gait,listing_lying,listing_thigh,listing_lumbar)
-
-    (d1,d2,d3)=preprocessing(lumbar_acc)
-    # {'acc': acc, 'v': v, 'ml': ml, 'ap': ap},
-    # {'gyro': gyro, 'yaw': yaw, 'pitch': pitch, 'roll': roll},
-    # {'magnitude': a_mag, 'fs': fs}
-
-    (s_start_pt, s_end_pt, ix_stillnes)=find_stillness(d3['magnitude'], d3['fs'], lying_vec_lumbar)
-
-
-    (locs, sin_theta_pks)=find_pt_with_theta(d2['pitch'], d3['fs'])
-
-    clean_pt(locs,ix_stillnes, d3['fs'],s_start_pt,s_end_pt)
-
-
-    (sit_2_stand, stand_2_sit)=pt_detection(locs, fs, acc, gyro, pitch, fuse, sin_theta_pks, draw)
-
-    pt=delete_PT_next_to_lying(pt,lying_vec_lumbar, ix_stillnes,walking_vec_lumbar,fs=100)
-
-    pt=elimination1(pt,ix_stillnes,walking_vec_lumber)
-
-
-    pt=sitting_segements_fitting_standards(walking_vec_lumbar, pt, ix_stillnes,fs)
-
-    #Elimination2 #428-437
-    (s2sit_index, s2stand_index)=find_sit_to_stand(sitting_vec)
-
-    # delete still periods #452-488
-
-    (s2sit_sensitivity, s2sit_precision, s2stand_sensitivity, s2stand_precision, sitting_acc)=performance_detection(pt, s2sit_index, s2stand_index,ix_stillness, lying_vec_lumbar,sitting_vec, fs)
+from src.transition_detection.statistics import performance_detection
+from src.transition_detection.elimination2 import elimination2
+from src.transition_detection.delete_still_periods import delete_still_periods
 
 
 
+def transition_detection(listing_acc_path:str,listing_gait_path:str ,listing_lying_path:str):
+    """ Transition_detection
+        Parameters
+        ----------
+        listing_acc_path: path of the 3D gyroscopes and 3D accelerometers files
+        listing_gait_path:  path of the gait files
+        listing_lying_path: path of the lying files
+
+        Returns
+        -------
+        pt: posture transition array
+        s2sit_sensitivity: detection sensitivity for stand-to-sit transitions
+        s2sit_precision: detection precision for stand-to-sit transitions
+        s2stand_sensitivity: detection sensitivity for sit-to-stand transitions
+        s2stand_precision: detection precision for sit-to-stand transitions
+    """
+
+
+    #Create files lists
+    (listing_acc, listing_gait, listing_lying, listing_lumbar, listing_thigh)= files_lists(listing_acc_path, listing_gait_path, listing_lying_path)
+
+    for ff in range(len(listing_lumbar)): #for each subject
+
+        #load the data (1-18)
+        (lumbar_acc, walking_vec_Lumbar, lying_vec_lumbar, thigh_acc, walking_vec_thigh, laying_vec_thigh) = load_data(ff, listing_acc_path, listing_gait_path, listing_lying_path, listing_acc,listing_gait, listing_lying, listing_thigh, listing_lumbar)
+
+        # preprocessing  (21-54)
+        (d1,d2,d3)=preprocessing(lumbar_acc)
+        # {'acc': acc, 'v': v, 'ml': ml, 'ap': ap},
+        # {'gyro': gyro, 'yaw': yaw, 'pitch': pitch, 'roll': roll},
+        # {'magnitude': a_mag, 'fs': fs}
+
+        # find stillness periods (55-106)
+        (s_start_pt, s_end_pt, ix_stillnes)=find_stillness(d3['magnitude'], d3['fs'], lying_vec_lumbar)
+
+        # find suspected postural transitions (PT) points according to Theta (tilt angle) (152-168)
+        (locs, sin_theta_pks)=find_pt_with_theta(d2['pitch'], d3['fs'])
+
+        # Clean PT with no stillness before/after (169-186)
+        locs=clean_pt(locs, ix_stillnes, d3['fs'], s_start_pt, s_end_pt)
+
+        # Postral Transition detection (234-338)
+        (sit_2_stand, stand_2_sit)=pt_detection(locs, fs, acc, gyro, pitch, fuse, sin_theta_pks, draw)
+
+        # Delete PT next to lying segments (334-388)
+        pt=delete_PT_next_to_lying(pt, lying_vec_lumbar, ix_stillnes, walking_vec_lumbar, fs)
+
+        # Elimination1 (389-413)
+        pt=elimination1(pt, ix_stillnes, walking_vec_lumber)
+
+        # Keep sitting segements that fit standards of length & stillness (415-427)
+        pt=sitting_segements_fitting_standards(walking_vec_lumbar, pt, ix_stillnes, fs)
+
+        # Elimination2 (428-437)
+        pt=elimination2(pt, fs)
+
+        # find transitions from sit to stand (439-450)
+        (s2sit_index, s2stand_index)=find_sit_to_stand(sitting_vec)
+
+        # delete still periods (452-488)
+        sitting_vec=delete_still_periods(s2sit_index, walking_vec_lumbar, s2stand_index, ix_stillnes, lying_vec_lumbar, fs)
+
+        # Statistics and Performance  estimation (489-545)
+        (s2sit_sensitivity, s2sit_precision, s2stand_sensitivity, s2stand_precision, sitting_acc)=performance_detection(pt, s2sit_index, s2stand_index,ix_stillness, lying_vec_lumbar,sitting_vec, fs)
+
+
+    return pt,s2sit_sensitivity, s2sit_precision, s2stand_sensitivity, s2stand_precision
 
 
 
@@ -74,37 +92,5 @@ for ff in range(len(listing_lumbar)):
 
 
 
-# for loop for every file in the main script
-#load the data #1-18 #Eden
 
-#preprocessing  #21-54 #Maayan
-
-# find stillness periods #55-106 #shaked
-
-#find suspected postural transitions (PT) points according to Theta (tilt angle) #152-168 #chen
-
-
-#Clean PT with no stillness before/after #169-186
-
-
-#Postral Transition detection #234-338
-    #  fuse = imufilter #234 maybe main
-
-#    PT = sortrows([Sit2Stand;Stand2Sit],1); #main 352
-
-#   Delete PT next to lying segments #354-388
-
-# Elimination1 #389-413
-
-#  Keep sitting segements that fit standards of length & stillness #415-427
-
-#Elimination2 #428-437
-
-#find transitions from sit to stand #439-450
-
-# delete still periods #452-488
-
-# Statistics and Performance  estimation  #489-545
-
-# Figures #546-594
 
