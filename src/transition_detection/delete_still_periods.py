ac1e5d5085413acb97714e41d5e01793c0a08eb7
khenheller <khenheller@mail.tau.ac.il>
2022-07-07 15:41:47 +0300

    Added elimination2

--- src/transition_detection/delete_still_periods.py
+++ src/transition_detection/delete_still_periods.py
@@ -37,15 +37,6 @@ def delete_still_periods(
             delete_ind = [[delete_ind], [i]]
     s2sit_index[delete_ind] = []
     s2stand_index[delete_ind] = []
-    # deleteInd = [];
-    # for jj = 1:length(s2sit_index)
-    #     walking = sum(Walking_Vec_Lumbar(s2sit_index(jj):s2stand_index(jj))) / length(s2sit_index(jj):s2stand_index(jj));
-    #     if walking > 0.1
-    #         deleteInd = [deleteInd ; jj];
-    #     end
-    # end
-    # s2sit_index(deleteInd) = [];
-    # s2stand_index(deleteInd) = [];
 
 # Detect still periods above 5min (but not lying) and mark them as sitting (can't be standing)
     delete_ind = []
@@ -62,33 +53,10 @@ def delete_still_periods(
             delete_ind = [[delete_ind] , [jj]]
     s2stand_index[delete_ind] = []
     s2sit_index[delete_ind] = []        
-    # deleteInd = [];
-    # for jj = 1:length(s2sit_index)-1
-    #     sigInd = s2stand_index(jj):s2sit_index(jj+1);
-    #     stillness = sum(ix_stillnes(sigInd)) / length(sigInd);
-    #     walking = sum(Walking_Vec_Lumbar(sigInd)) / length(sigInd);
-    #     Lying =  sum(Lying_Vec_Lumbar(sigInd)) / length(sigInd);
-    #     if length(sigInd) < 5*60*fs & stillness > 0.95 & walking ==0 & Lying==0
-    #         deleteInd = [deleteInd ; jj];
-    #     end
-    #     if length(sigInd) < 10*fs  % delete standing segments shorter than 10 sec between PTs
-    #         deleteInd = [deleteInd ; jj];
-    #     end
-    #     if Lying > 0 % delete false sitting periods that are actually lying
-    #         deleteInd = [deleteInd ; jj];
-    #     end
-    # end
-    #s2stand_index(deleteInd) = [];
-    #s2sit_index(deleteInd+1) = [];
 
     sitting_vec = np.zeros(np.shape(sitting_vec))
     for k in len(s2sit_index):
         sitting_vec[s2sit_index[k] : s2stand_index[k]] = 1*(s2stand_index[k] - s2sit_index[k])
     sitting_vec[lying_vec_lumbar == 1] = 0   
-    # Sitting_Vec = zeros(size(Sitting_Vec));
-    # for kk=1:length(s2sit_index)
-    #     Sitting_Vec(s2sit_index(kk):s2stand_index(kk)) = 1;
-    # end
-    # Sitting_Vec(Lying_Vec_Lumbar==1) = 0;
 
     return sitting_vec
